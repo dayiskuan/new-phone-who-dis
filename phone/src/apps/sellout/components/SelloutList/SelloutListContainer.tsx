fc211acb98423ac61616dce52668442a1c57664d
Christopher <onlygamernerd@gmail.com>
2021-01-03 21:35:21 +0100

    feat(listing/nui): hide/show on modal status

--- phone/src/apps/sellout/components/SelloutList/SelloutListContainer.tsx
+++ phone/src/apps/sellout/components/SelloutList/SelloutListContainer.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import { useListing } from "../../hooks/useListing";
 import { SelloutList } from "./SelloutList";
+import SelloutSearch from "../search/SelloutSearch"
+import {useListingModal} from "../../hooks/useListingModal";
+
+import { makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  hidden: {
+    display: 'none'
+  },
+  show: {
+    display: 'block'
+  }
+}))
 
 export const SelloutListContainer = () => {
+  const { modal } = useListingModal()
   const { listing } = useListing();
-  return <SelloutList listings={listing} />;
+
+  const classes = useStyles();
+  return (
+    <div className={modal ? classes.hidden : classes.show}>
+      <SelloutSearch />
+      <SelloutList listings={listing} />
+    </div>
+  )
 };
