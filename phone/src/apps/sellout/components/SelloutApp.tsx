f29b9d2be644324a3ca379e2beb74a00987aa7fd
Christopher <onlygamernerd@gmail.com>
2021-01-03 21:32:03 +0100

    removed certian css stuff

--- phone/src/apps/sellout/components/SelloutApp.tsx
+++ phone/src/apps/sellout/components/SelloutApp.tsx
@@ -3,23 +3,18 @@ import { AppWrapper } from "../../../ui/components/AppWrapper";
 import { AppContent } from "../../../ui/components/AppContent";
 import { SelloutTitle } from "./SelloutTitle";
 import { SelloutListContainer } from "./SelloutList/SelloutListContainer";
-import { NavigationBar } from "./navigation/NavigationBar";
-import { Switch, Route } from "react-router-dom";
 
 import "./Sellout.css";
-import { ListingFormContainer } from "./form/ListingFormContainer";
+import {ListingForm} from "./form/ListingForm";
 
 export const SelloutApp = () => {
   return (
     <AppWrapper id="sellout-app">
       <SelloutTitle />
       <AppContent>
-        <Switch>
-          <Route path="/sellout" exact component={SelloutListContainer} />
-          <Route path="/sellout/new" component={ListingFormContainer} />
-        </Switch>
+        <ListingForm />
+        <SelloutListContainer />
       </AppContent>
-      <NavigationBar />
     </AppWrapper>
   );
 };
